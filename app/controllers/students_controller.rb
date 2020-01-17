e8143d12c9947574cfe826b691a28eb80e04d5b2
Baktiar Miah <baktiar.m@gmail.com>
2020-01-17 17:11:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
 
   def index
-    @students = Student.all
-  end
-
+          @students = Student.all
+          render '/student/index'
+      end
 end
